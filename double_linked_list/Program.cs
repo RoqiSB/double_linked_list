038d69bf1fe1b6732644bab540c67aeb33859a84
Roqi SB <roqisb9@gmail.com>
2022-11-14 19:52:28 +0700

    add class program

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -137,28 +137,21 @@ namespace double_linked_list
         }
         public void descending()
         {
-            if(listEmpty())
+            if (listEmpty())
                 Console.WriteLine("\nList is empty");
             else
-            Console.WriteLine("\nRecord in the Descending order of" + "roll number are: \n");
+                Console.WriteLine("\nRecord in the Descending order of" + "roll number are: \n");
             Node currentNode;
             for (currentNode = START; currentNode != null; currentNode = currentNode.next)
             { }
 
-            while(currentNode != null)
+            while (currentNode != null)
             {
                 Console.Write(currentNode.noMhs + "" + currentNode.name + "\n");
-
+                currentNode = currentNode.prev;
             }
         }
     }
-       
-        
-    }
-
-
-
-
 
 
 
@@ -166,6 +159,21 @@ namespace double_linked_list
     {
         static void Main(string[] args)
         {
+            DoubleLinkedList obj = new DoubleLinkedList();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\nMenu");
+                    Console.WriteLine("1. Add a record to the list");
+                    Console.WriteLine("2. Delete a record from the list");
+                    Console.WriteLine("3. View all records in the ascending order of roll numbers");
+                    Console.WriteLine("4. View all records in the descending order of roll numbers");
+                    Console.WriteLine("5. Search for a record in the list");
+                    Console.WriteLine("6. Exit");
 
+                }
+            }
         }
     }
+}
