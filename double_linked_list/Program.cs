c25fa2b53a413691039fb0c48da3dd8f43c377b7
Roqi SB <roqisb9@gmail.com>
2022-11-14 20:14:48 +0700

    add case 5

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -207,6 +207,25 @@ namespace double_linked_list
                             }
                             break;
                         case '5':
+                            {
+                                if (obj.listEmpty() == true)
+                                {
+                                    Console.WriteLine("\nList is empty");
+                                    break;
+                                }
+                                Node prev, curr;
+                                prev = curr = null;
+                                Console.WriteLine("\nEnter the roll number of the student whose record you want to search: ");
+                                int num = Convert.ToInt32(Console.ReadLine());
+                                if (obj.search(num, ref prev, ref curr) == false)
+                                    Console.WriteLine("\nRecord not found");
+                                else
+                                {
+                                    Console.WriteLine("\nRecord found");
+                                    Console.WriteLine("\nRoll number: " + curr.noMhs);
+                                    Console.WriteLine("\nName : "+ curr.name);
+                                }
+                            }
                     }
                 }   
             }
