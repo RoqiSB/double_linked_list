b773dc535dd6a0ab5bcd84d412127b0653fb3598
Roqi SB <roqisb9@gmail.com>
2022-11-14 16:57:19 +0700

    create method  addNode

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace double_linked_list
 {
@@ -10,6 +11,31 @@ namespace double_linked_list
         //in terms of next and previous
         public int noMhs;
         public string name;
+        //point to the succeding node
+        public Node next;
+        //point to the precceeding node
+        public Node prev;
+    }
+    class DoubleLinkedList
+    {
+        Node START;
+        
+        //construktor
+        public DoubleLinkedList()
+        {
+            START = null;
+        }
+        public void addNode()
+        {
+            int nim;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student :");
+            nm = Console.ReadLine();
+            Node newNode = new Node();
+            newNode.noMhs = nim;
+        }
     }
 
 
