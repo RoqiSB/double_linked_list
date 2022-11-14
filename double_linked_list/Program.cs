33b07811b8207234b2237f4602c658874eed241e
Roqi SB <roqisb9@gmail.com>
2022-11-14 16:50:44 +0700

    add int and string in class Node

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -2,5 +2,27 @@
 
 namespace double_linked_list
 {
+    class Node
+    {
+        //Node class reoresents the node of doubly linked list.
+        //it consists of the information part and links to 
+        //its succeeding and preceeding
+        //in terms of next and previous
+        public int noMhs;
+        public string name;
+    }
 
+
+
+
+
+
+    
+    class Program
+    {
+        static void Main(string[]args)
+        {
+
+        }
+    }
 }
